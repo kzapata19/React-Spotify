234af419dd6680a80e5dd342f011338162f9a805
Karen Zapata <kzapata0919@gmail.com>
2017-04-10 23:36:58 -0700

    Add state to click button so number is updated for every click

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Button from './button';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        counter: 1,
+      }
+  }
+
   render() {
     return (
       <div>
-        Hi
+      {this.state.counter}
+<Button name={'Add 1'} clicked={() => this.setState({ counter: this.state.counter + 1 })} />
       </div>
     );
   }
